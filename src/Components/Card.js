98cfbc97e069a3e3130979fac3576158d1875c4f
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-09-01 23:44:26 +0545

    working on responsiveness

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -10,10 +10,9 @@ const Card = () => {
         <div className="card-img">
           <img src={img} alt="img" />
         </div>
-        <div className="card-header">
-          <h5>Attention to detail</h5>
-        </div>
+
         <div className="card-info">
+          <h5>Attention to detail</h5>
           <p>
             We look for attention to detail in our candidates.As a frontend
             intern, you should have superpowers of looking into details of a
