1c0043bd8eab727a9feebf447f118e9c6138d847
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-09-01 12:57:14 +0545

    homepage updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -70,48 +70,76 @@ const Home = () => {
           </div>
         </div>
       </div>
-
-      <div className="news">
-        <div className="card">
-          <h5>resources</h5>
-          <div className="card-links">
-            <ul>
-              <li>Designer News</li>
-              <hr className="hr-separator" />
-              <li>Smashing Magazine</li>
-              <hr className="hr-separator" />
-              <li>Frontend Front</li>
-              <hr className="hr-separator" />
-              <li>View all Resources</li>
-            </ul>
+      <div className="news-wrapper">
+        <div className="news">
+          <div className="card">
+            <h5>resources</h5>
+            <div className="card-links">
+              <ul>
+                <li>Designer News</li>
+                <hr className="hr-separator" />
+                <li>Smashing Magazine</li>
+                <hr className="hr-separator" />
+                <li>Frontend Front</li>
+                <hr className="hr-separator" />
+                <li>View all Resources</li>
+              </ul>
+            </div>
+          </div>
+          <div className="separator"></div>
+          <div className="card">
+            <h5>Latest from frontend</h5>
+            <div className="card-links">
+              <ul>
+                <li>Javascript Performance Optimization Tips:An overview</li>
+                <hr className="hr-separator" />
+                <li>Javascript Performance Optimization Tips:An overview</li>
+                <hr className="hr-separator" />
+                <li>Javascript Performance Optimization Tips:An overview</li>
+                <hr className="hr-separator" />
+                <li>Javascript Performance Optimization Tips:An overview</li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="separator"></div>
-        <div className="card">
-          <h5>Latest from frontend</h5>
-          <div className="card-links">
-            <ul>
-              <li>Javascript Performance Optimization Tips:An overview</li>
-              <hr className="hr-separator" />
-              <li>Javascript Performance Optimization Tips:An overview</li>
-              <hr className="hr-separator" />
-              <li>Javascript Performance Optimization Tips:An overview</li>
-              <hr className="hr-separator" />
-              <li>Javascript Performance Optimization Tips:An overview</li>
-            </ul>
-          </div>
+        <div className="separator-logo">
+          <hr className="hr-small" />
+
+          <img src={separatorLogo} alt="logo" />
+          <hr className="hr-small" />
         </div>
       </div>
-      <div className="separator-logo">
-        <hr className="hr-small" />
 
-        <img src={separatorLogo} alt="logo" />
-        <hr className="hr-small" />
-      </div>
       <div className="gallery">
         <div className="title">
           <h2>Some random food photos</h2>
           <p>Just because</p>
+        </div>
+        <div className="img-container">
+          <img
+            src="https://images.unsplash.com/photo-1558467523-46113f1fcf72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
+            alt="food1"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1558920558-fb0345e52561?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+            alt="food1"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1576402187878-974f70c890a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1190&q=80"
+            alt="food1"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1513262599279-d287e25f4d84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1002&q=80"
+            alt="food1"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1568581789190-ae90a7da930b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+            alt="food1"
+          />
+          <img
+            src="https://images.unsplash.com/photo-1542002976-c3be9b105508?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1109&q=80"
+            alt="food1"
+          />
         </div>
       </div>
     </>
